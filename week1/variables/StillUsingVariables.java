cddf14df6477d6586998373dcd7f846676db8b68
Kohei Asai <me@axross.io>
2019-02-22 10:20:08 -0800

    Still Using Variables
    https://programmingbydoing.com/a/still-using-variables.html

--- week1/variables/StillUsingVariables.java
+++ week1/variables/StillUsingVariables.java
@@ -1,5 +1,14 @@
 package week1.variables;
 
-public class StillUsingVariables {
+import java.time.LocalDate;
 
+public class StillUsingVariables {
+    public static void main(String[] args) {
+        String firstName = "Kohei";
+        String lastName = "Asai";
+        LocalDate graduatedAt = LocalDate.of(2020, 12, 31);
+
+        System.out.println(
+                String.format("My name is %s %s and I'll graduate in %d.", firstName, lastName, graduatedAt.getYear()));
+    }
 }
