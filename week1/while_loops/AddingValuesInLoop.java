893dd0d398be64ec1f266ad0aea5eea184cdf844
Kohei Asai <me@axross.io>
2019-02-23 00:23:37 -0800

    AddingValuesInLoop

--- week1/while_loops/AddingValuesInLoop.java
+++ week1/while_loops/AddingValuesInLoop.java
@@ -1,5 +1,35 @@
 package week1.while_loops;
 
-public class AddingValuesInLoop {
+import java.util.Scanner;
 
+public class AddingValuesInLoop {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("I will add up the numbers you give me.");
+
+        int total = 0;
+
+        while (true) {
+            System.out.print("Number: ");
+
+            int number = reader.nextInt();
+
+            reader.nextLine();
+
+            if (number == 0) {
+                break;
+            }
+
+            total += number;
+
+            System.out.println(String.format("The total so far is %d.", total));
+        }
+
+        System.out.println("");
+
+        System.out.println(String.format("The total is %d.", total));
+
+        reader.close();
+    }
 }
